9baf7f3d2330d89dede874db224cb690302189d2
KWangechi <rachel.karanja@strathmore.edu>
2025-01-15 16:14:14 +0300

    docs: moved server.js to the root folder location

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const startConnection = require("./config/database");
+const startConnection = require("./src/config/database");
 
 const PORT = process.env.PORT || 5000;
 
 startConnection();
 
 // CORS Handler
-const corsHandler = require("./middlewares/corsHandler");
+const corsHandler = require("./src/middlewares/corsHandler");
 app.use(corsHandler);
 
 // allow Parsing of JSON payloads to routes
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 // import routes
-const allRoutes = require("./routes/index");
+const allRoutes = require("./src/routes/index");
 app.use("/api/v1", allRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
